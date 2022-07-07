40cfb8c7a9b86a91bd09d7538304075f154b2daf
Pacifique Kwizera <kwizerapacifique19@gmail.com>
2022-07-07 21:24:42 +0200

    lesson 26

--- dojo-blog/src/useFetch.js
+++ dojo-blog/src/useFetch.js
@@ -1,30 +1,38 @@
-import { useEffect,useState } from "react";
+import { useEffect, useState } from "react";
 
 const useFetch = (url) => {
-    const [data, setData] = useState(null);
-    const [isPending, setPending]=useState(true);
-    const [error,setError]=useState(null);
+  const [data, setData] = useState(null);
+  const [isPending, setPending] = useState(true);
+  const [error, setError] = useState(null);
 
-    useEffect(() => {
-        fetch(url)
-          .then((res) => {
-            if(!res.ok){
-              throw Error("Attempt to fetch data failed!");
-            }
-            return res.json();
-          })
-          .then((response_data) => {
-            setData(response_data);
-            setPending(false);
-            setError(null);
-          })
-          .catch(err=>{
-            setError(err.message);
-            setPending(false);
-          });
-      }, [url]);
+  useEffect(() => {
+    const abortCont = new AbortController();
 
-      return {data, isPending, error}
-}
- 
+    fetch(url, { signal: abortCont.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("Attempt to fetch data failed!");
+        }
+        return res.json();
+      })
+      .then((response_data) => {
+        setData(response_data);
+        setPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted!");
+        } else {
+          setError(err.message);
+          setPending(false);
+        }
+      });
+
+    return () => abortCont.abort();
+  }, [url]);
+
+  return { data, isPending, error };
+};
+
 export default useFetch;
